c97bf53b6f6298e19a5c028371f67909cae60430
Dugas, Jeff <jeff.dugas@stelligent.com>
2018-02-07 17:47:47 -0500

    Add support for SEC-116

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Clokta enables you authenticate into an AWS account using Okta on the command li
 ```bash
 > pip3 install clokta
 ```
+
 You will need Python 2.7 or 3 installed on your machine
 
 ## To Use
@@ -18,7 +19,7 @@ You will need Python 2.7 or 3 installed on your machine
 
 1. Ensure you have Google Authenticator setup in Okta.  Currently, Google Authenticator is the only MFA supported.
 
-2. Obtain a clokta.yml from your team that contains the required information for you account.
+1. Obtain a clokta.yml from your team that contains the required information for you account.
    (See below for how a team can generate a clokta.yml)
 
    A typical clokta.yml will look like this.
@@ -29,16 +30,16 @@ OKTA_AWS_APP_URL: https://washpost.okta.com/home/amazon_aws/0of1f11ff1fff1ffF1f1
 OKTA_AWS_ROLE_TO_ASSUME: arn:aws:iam::111111111111:role/Okta_Role
 OKTA_IDP_PROVIDER: arn:aws:iam::111111111111:saml-provider/Okta_Idp
 OKTA_USERNAME:
+MULTIFACTOR_PREFERENCE: OKTA-sms
 ```
 
 Note: You can insert your username into the yml file or clokta will prompt you for it
 
 Note: Alternatively, you can put all these values in your environment rather than specifying the yml file on the commandline.
 
-3. Run AWS commands for the next hour.  
+1. Run AWS commands for the next hour.
 
    After an hour the keys expire and you must rerun clokta.
-
 
 ## Generating a clokta.yml for your team
 
@@ -50,7 +51,7 @@ Clokta needs the following information specific to your team AWS account:
   - This will always be 'washpost.okta.com'
 - OKTA_AWS_APP_URL
   - Go to your Okta dashboard, right click on your AWS tile (e.g. ARC-App-Myapp), and copy the link.  It will be something like:
-    https://washpost.okta.com/home/amazon_aws/0oa1f77g6u1hoarrV0h8/272?fromHome=true
+    `https://washpost.okta.com/home/amazon_aws/0oa1f77g6u1hoarrV0h8/272?fromHome=true`
   - Strip off the "?fromHome=true" and that is your OKTA_AWS_APP_URL
 - OKTA_AWS_ROLE_TO_ASSUME
   - Go to your AWS Account with the web console.
@@ -70,9 +71,32 @@ OKTA_ORG: washpost.okta.com
 OKTA_AWS_APP_URL: fill in value
 OKTA_AWS_ROLE_TO_ASSUME: fill in value
 OKTA_IDP_PROVIDER: fill in value
-OKTA_USERNAME:
+OKTA_USERNAME: fill in value
 ```
 
 Note: The "OKTA_USERNAME" is just to make it easy for developers to put their name in when they get the file and not have to be prompted for it every time.  Leave it blank.
 
 Give this to all team members.
+
+## Recent updates
+
+### SEC-115
+
+> Extend duration of temp credentials 24 hours or as long as possible
+
+According to [the boto3 documentation](http://boto3.readthedocs.io/en/latest/reference/services/sts.html#STS.Client.assume_role_with_saml):
+
+<pre>
+* DurationSeconds (integer) --
+The duration, in seconds, of the role session. The value can range from 900 seconds (15 minutes) to 3600 seconds (1 hour). By default, the value is set to 3600 seconds. An expiration can also be specified in the SAML authentication response's SessionNotOnOrAfter value. The actual expiration time is whichever value is shorter.
+</pre>
+
+Given maximum value is also the default duration, the session willneed to be re-freshed after 3600 seconds (1 hour).
+
+### SEC-116
+
+> Support other MFA options, SMS and Okta Verify
+
+These are now all supported. If ou don't have a value for `MULTIFACTOR_PREFERENCE` in your Okta config file, and you don't know what to put there, simply press enter when prompted for it. The tool will prompt you to choose one from a set of options, and you can copy/paste your preferred option into your config file.
+
+> NOTE: Activating the MFA types must be done though the Okta user interface.
--- cli/role.py
+++ cli/role.py
@@ -4,16 +4,17 @@ This is the entry-point to the cli application.
 import copy
 import getpass
 import json
+import logging
 import os
 import sys
 from datetime import date, datetime
 
 import boto3
-import requests
-
 import click
+import requests
 import yaml
 from bs4 import BeautifulSoup
+
 from six.moves import input
 
 
@@ -21,98 +22,169 @@ from six.moves import input
 @click.option('--verbose', '-v', is_flag=True, help='Show detailed .')
 @click.option('--config', '-c', default=None, help='Path to configuration file.')
 def assume_role(config=None, verbose=False):
-    configuration = load_configuration(config_path=config, verbose=verbose)
-    if verbose == True:
+    ''' entry point for the cli tool '''
+    configuration = __load_configuration(config_path=config, verbose=verbose)
+    if verbose:
         copy_config = copy.deepcopy(configuration)
         copy_config['OKTA_PASSWORD'] = '<redacted>'
         click.echo('Configuration:')
         click.echo(copy_config)
 
-    session_token = okta_session_token(configuration=configuration)
-    if verbose == True:
+    session_token = __okta_session_token(configuration=configuration, verbose=verbose)
+    if verbose:
         click.echo('Okta session token:')
         click.echo(session_token)
 
-    saml_assertion = saml_assertion_aws(session_token=session_token, configuration=configuration)
-    if verbose == True:
+    saml_assertion = __saml_assertion_aws(session_token=session_token, configuration=configuration)
+    if verbose:
         click.echo('SAML:')
         click.echo(saml_assertion)
 
     client = boto3.client('sts')
+
     response = client.assume_role_with_saml(
         RoleArn=configuration['OKTA_AWS_ROLE_TO_ASSUME'],
         PrincipalArn=configuration['OKTA_IDP_PROVIDER'],
         SAMLAssertion=saml_assertion
     )
-    if verbose == True:
+    if verbose:
         click.echo('assume_role_with_saml:')
         click.echo(response)
 
-    write_aws_configs(role_credentials=response, verbose=verbose)
+    __write_aws_configs(role_credentials=response, verbose=verbose)
 
-def load_configuration(config_path, verbose=False):
+def __load_configuration(config_path, verbose=False):
     ''' Configuration file values override environment variables '''
     cfg = {}
     configuration = {}
     if config_path:
-        if not os.path.isfile(config_path):
-            raise ValueError("No such file '{}'".format(config_path))            
-        else:
+        try:
             with open(config_path, 'r') as config_file:
                 contents = config_file.read()
                 cfg = yaml.load(contents, Loader=yaml.SafeLoader)
+        except OSError as oserr:
+            msg = 'Error related to your configuration file: {}'.format(oserr)
+            logging.exception(msg) if verbose else print(msg)
+            sys.exit(1)
+        except Exception as err:
+            msg = 'Unexpected error: {}'.format(err)
+            logging.exception(msg) if verbose else print(msg)
+            sys.exit(1)
 
     for key in [
-            'OKTA_USERNAME', 'OKTA_PASSWORD', 'OKTA_ORG', 'OKTA_AWS_APP_URL', 'OKTA_AWS_ROLE_TO_ASSUME', 'OKTA_IDP_PROVIDER'
+            'OKTA_USERNAME',
+            'OKTA_PASSWORD',
+            'OKTA_ORG',
+            'OKTA_AWS_APP_URL',
+            'OKTA_AWS_ROLE_TO_ASSUME',
+            'OKTA_IDP_PROVIDER',
+            'MULTIFACTOR_PREFERENCE'
     ]:
         if key.endswith('PASSWORD'):
-            configuration[key] = cfg.get(key) if cfg.get(key) else os.getenv(key, getpass.getpass(prompt="Enter a value for {}:".format(key)))
+            configuration[key] = os.getenv(key, getpass.getpass(prompt="Enter a value for {}:".format(key)))
         else:
-            configuration[key] = cfg.get(key) if cfg.get(key) else os.getenv(key, input("Enter a value for {}:".format(key)))
+            configuration[key] = cfg.get(key) if cfg.get(key) \
+                else os.getenv(key, input("Enter a value for {}:".format(key)))
 
     return configuration
 
-def okta_session_token(configuration):
-    session_token = None
+def __okta_session_token(configuration, verbose=False):
+    ''' Authenticate with Okta to create a session and receive a session token '''
     okta_response = None
 
     try:
-        okta_response = okta_auth_response(configuration=configuration)
-    except requests.exceptions.HTTPError as err:
-        print('Okta returned this credentials/password related error: {}'.format(err))
-        exit(1)
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
-        exit(1)
+        okta_response = __okta_auth_response(configuration=configuration)
+    except requests.exceptions.HTTPError as http_err:
+        msg = 'Okta returned this credentials/password related error: {}'.format(http_err)
+        logging.exception(msg) if verbose else print(msg)
+        sys.exit(1)
+    except Exception as err:
+        msg = 'Unexpected error: {}'.format(err)
+        logging.exception(msg) if verbose else print(msg)
+        sys.exit(1)
 
     if okta_response['status'] == 'MFA_REQUIRED':
-        # TODO: query user for MFA token, re-validate session token
-        factors = [
-            fact for fact in okta_response['_embedded']['factors'] if fact['factorType'] == 'token:software:totp' and fact['provider'] == 'GOOGLE'
-        ]
-        if factors:
-            factor = factors[0]
-            state_token = okta_response['stateToken']
-            otp_value = input("Enter your Google Authenticator token:")
-            try:
-                mfa_response = okta_mfa_verification(
-                    factor_dict= factor,
-                    state_token=state_token,
-                    otp_value=otp_value
-                )
-                session_token = mfa_response['sessionToken']
-            except requests.exceptions.HTTPError as err:
-                print('Okta returned this MFA related error: {}'.format(err))
-                exit(1)
-            except:
-                print("Unexpected error:", sys.exc_info()[0])
-                exit(1)
+        return __okta_session_token_mfa(
+            auth_response=okta_response,
+            factors=okta_response['_embedded']['factors'],
+            factor_preference=configuration['MULTIFACTOR_PREFERENCE'],
+            verbose=verbose
+        )
     else:
-        session_token = okta_response['sessionToken']
+        return okta_response['sessionToken']
+
+def __okta_session_token_mfa(auth_response, factors, factor_preference, verbose=False):
+    ''' Determine which factor to use and apply it to get a session token '''
+    factor = __choose_factor(
+        factors=factors,
+        factor_preference=factor_preference,
+        verbose=verbose
+    )
+    state_token = auth_response['stateToken']
+
+    if factor['factorType'] == 'sms':
+        # TODO: ask for it to send us an MFA token
+        send_sms_response = __okta_mfa_verification(
+            factor_dict=factor,
+            state_token=state_token,
+            otp_value=None
+        )
+
+    otp_value = input('Enter your multifactor authentication token: ')
+    try:
+        mfa_response = __okta_mfa_verification(
+            factor_dict=factor,
+            state_token=state_token,
+            otp_value=otp_value
+        )
+        session_token = mfa_response['sessionToken']
+    except requests.exceptions.HTTPError as http_err:
+        msg = 'Okta returned this MFA related error: {}'.format(http_err)
+        logging.exception(msg) if verbose else print(msg)
+        sys.exit(1)
+    except Exception as err:
+        msg = 'Unexpected error: {}'.format(err)
+        logging.exception(msg) if verbose else print(msg)
+        sys.exit(1)
 
     return session_token
 
-def okta_mfa_verification(factor_dict, state_token, otp_value):
+def __choose_factor(factors, factor_preference=None, verbose=False):
+    ''' Automatically choose, or allow user to choose, the MFA option '''
+    if len(factors) == 1:
+        if verbose:
+            print('Using available factor: {}-{}'.format(
+                factors[0]['provider'],
+                factors[0]['factorType'],
+            ))
+        return factors[0]
+
+    if factor_preference:
+        matched_factors = [
+            fact for fact in factors \
+                if '{}-{}'.format(fact['provider'], fact['factorType']) == factor_preference
+        ]
+        if matched_factors:
+            if verbose:
+                print('Using preferred factor: {}'.format(factor_preference))
+            return matched_factors[0]
+
+    choice = 1
+    index = 1
+    for fact in factors:
+        print(
+            '{index} - {provider}-{factor_type}'.format(
+                index=index,
+                provider=fact['provider'],
+                factor_type=fact['factorType']
+            )
+        )
+        index += 1
+    choice = int(input('Choose a MFA type to use:'))        
+    return factors[choice - 1]
+
+def __okta_mfa_verification(factor_dict, state_token, otp_value=None):
+    ''' Sends the  '''
     url = factor_dict['_links']['verify']['href']
     headers = {
         'Accept': 'application/json',
@@ -120,9 +192,10 @@ def okta_mfa_verification(factor_dict, state_token, otp_value):
         'Cache-Control': '"no-cache'
     }
     payload = {
-        'stateToken': state_token,
-        'answer': otp_value
+        'stateToken': state_token
     }
+    if otp_value:
+        payload['answer'] = otp_value
 
     response = requests.post(url, data=json.dumps(payload), headers=headers)
     if response.status_code == requests.codes.ok:
@@ -131,7 +204,8 @@ def okta_mfa_verification(factor_dict, state_token, otp_value):
     else:
         response.raise_for_status()
 
-def okta_auth_response(configuration):
+def __okta_auth_response(configuration):
+    ''' Returns an HTTP response for credentials-based authentication with Okta '''
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
@@ -150,18 +224,21 @@ def okta_auth_response(configuration):
     else:
         response.raise_for_status()
 
-def saml_assertion_aws(session_token, configuration):
-    response = okta_app_response(session_token=session_token, configuration=configuration)
-    # with open('saml_response.html', 'wb') as f:
-    #     f.write(response.content)
+def __saml_assertion_aws(session_token, configuration):
+    ''' fetch saml 2.0 assertion '''
+    response = __okta_app_response(session_token=session_token, configuration=configuration)
+
+    with open('saml_response.html', 'wb') as f:
+        f.write(response.content)
+
     soup = BeautifulSoup(response.content, "html.parser")
-    assertion = None 
+    assertion = None
     for inputtag in soup.find_all('input'):
-        if(inputtag.get('name') == 'SAMLResponse'):
+        if inputtag.get('name') == 'SAMLResponse':
             assertion = inputtag.get('value')
     return assertion
 
-def okta_app_response(session_token, configuration):
+def __okta_app_response(session_token, configuration):
     url = configuration['OKTA_AWS_APP_URL'] + '?onetimetoken=' + session_token
     response = requests.get(url)
     if response.status_code == requests.codes.ok:
@@ -169,18 +246,25 @@ def okta_app_response(session_token, configuration):
     else:
         response.raise_for_status()
 
-def write_aws_configs(role_credentials, verbose=False):
+def __write_aws_configs(role_credentials, verbose=False):
+    ''' Generates a shell file you can execute to apply role credentials to the environment '''
     output_file_name = 'aws_session.sh'
-    if verbose == True:
+    if verbose:
         print(json.dumps(obj=role_credentials, default=json_serial, indent=4, sort_keys=True))
 
     lines = [
-        'export AWS_ACCESS_KEY_ID={}\n'.format(role_credentials['Credentials']['AccessKeyId']),
-        'export AWS_SECRET_ACCESS_KEY={}\n'.format(role_credentials['Credentials']['SecretAccessKey']),
-        'export AWS_SESSION_TOKEN={}\n'.format(role_credentials['Credentials']['SessionToken'])
+        'export AWS_ACCESS_KEY_ID={}\n'.format(
+            role_credentials['Credentials']['AccessKeyId']
+        ),
+        'export AWS_SECRET_ACCESS_KEY={}\n'.format(
+            role_credentials['Credentials']['SecretAccessKey']
+        ),
+        'export AWS_SESSION_TOKEN={}\n'.format(
+            role_credentials['Credentials']['SessionToken']
+        )
     ]
-    with open(output_file_name, mode='w') as f:
-        f.writelines(lines)
+    with open(output_file_name, mode='w') as file_handle:
+        file_handle.writelines(lines)
     print(
         "AWS keys saved to {loc}. To use, 'source {loc}'".format(
             loc=output_file_name
@@ -188,7 +272,7 @@ def write_aws_configs(role_credentials, verbose=False):
     )
 
 def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
+    ''' JSON serializer for objects not serializable by default json code '''
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
     raise TypeError("Type %s not serializable" % type(obj))
