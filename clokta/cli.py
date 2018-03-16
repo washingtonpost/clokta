117bc7ecbac53fad05b06b3211d870dffe747040
Dugas, Jeff <jeff.dugas@stelligent.com>
2018-03-16 14:28:19 -0400

    Expose cli as library methods

---
+++ clokta/cli.py
@@ -0,0 +1,15 @@
+'''
+This is the entry-point to the cli application.
+'''
+import click
+
+from clokta.role_assumer import RoleAssumer
+
+
+@click.command()
+@click.option('--verbose', '-v', is_flag=True, help='Show detailed')
+@click.option('--profile', '-p', required=True, help='Configuration profile')
+def assume_role(profile, verbose=False):
+    ''' Click point of entry '''
+    assumer = RoleAssumer(profile=profile, verbose=verbose)
+    assumer.assume_role()
